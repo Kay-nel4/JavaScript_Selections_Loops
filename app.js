9c29051181f42a4280a9805121d9703d2b64ca95
Kayla Knelle <kaynell3234@gmail.com>
2026-01-15 19:01:48 -0500

    Resubmit of the JavaScript Selection Loops Exercise, fixed exercise 3 to print only odd numbers in each loop, fixed the for loop in exercise 4 to include let and also properly place the not found message, also used modulus checks in exercise 5 (the custom fizzbuzz check).

--- app.js
+++ app.js
@@ -35,17 +35,23 @@ for(let i = 1; i <= 100; i++)
 // Exercise 3 Section
 console.log("EXERCISE 3:\n==========\n");
 let num = 0;
-while(num <= 100){ 
-    console.log(num);
-    num++;
+while(num <= 100){
+        if(num %2 != 0){
+            console.log(num);    
+        } 
+        num++;
 }
 
-console.log("====================")
+console.log("====================");
+console.log("---- Do/While Loop----");
+console.log("====================");
 
 let number = 0;
 do{
+    if(number % 2 != 0){
+        console.log(number);
+    }
     number++;
-    console.log(number);
 }while(number < 100);
 
 console.log("FIZZBUZZ Loops");
@@ -93,17 +99,16 @@ console.log("-----------")
 let value = Math.round((Math.random() * 500));
 
 let n = Math.round(Math.random() * (500 - 100) + 100);
-for(i = 0; i = n; i++){
-    if(i != n)
-    {
-        console.log("Did Not Find Value!");
-       
-    }
-    else if(i == n)
+for(let i = 0; i = n; i++){
+    if(i == n)
     {
         console.log(`Found Value ${i}`);
         break;
+       
     }
+
+    console.log("Value Not Found.")
+
 }
 
 console.log("Exercise 5 ----Customized FIZZBUZZ----");
@@ -113,21 +118,19 @@ let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1);
 let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1);
 let j = Math.round(Math.random() * (1000 - 1) + 1);
 let start = Math.round(Math.random() * (10 - 1) + 1);
-
-for(i = start; i <= j; i++)
+for(let i = start; i <= j; i++)
 {
-    if(i == fizzDivisor)
+    if(i % fizzDivisor == 0)
     {
         console.log(`FIZZ ${i}`);
     }
-    else if(i == buzzDivisor)
+    else if(i % buzzDivisor == 0)
     {
         console.log(`BUZZ ${i}`);
     }
-    else
-    {
-        console.log("Sorry no FIZZ or BUZZ here!");
-    }
+    
+    console.log(`Sorry no FIZZ or BUZZ here! ${i}`);
+    
 }
 
 
